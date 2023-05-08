d79d8666d6ee851990293e09acb0dc2250aec301
Giovanni Poliko <60165179+gpoliko@users.noreply.github.com>
2023-05-08 17:47:57 +1200

    test: add bubble_sort tests

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,5 +1,13 @@
 pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
-    let len = arr.len();
+    if arr.is_empty() {
+        return;
+    }
+
+    let mut len = arr.len();
+    if len < 2 {
+        return;
+    }
+
     let mut swapped = true;
 
     while swapped {
@@ -13,3 +21,44 @@ pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
         len -= 1;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_array() {
+        let mut arr: [i32; 0] = [];
+        bubble_sort(&mut arr);
+        assert_eq!(arr, []);
+    }
+
+    #[test]
+    fn test_single_element_array() {
+        let mut arr = [5];
+        bubble_sort(&mut arr);
+        assert_eq!(arr, [5]);
+    }
+
+    #[test]
+    fn test_sorted_array() {
+        let mut arr = [1, 2, 3, 4, 5];
+        bubble_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_unsorted_array() {
+        let mut arr = [5, 2, 3, 1, 4];
+        bubble_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_large_array() {
+        let mut arr = [10, 5, 8, 3, 2, 6, 4, 7, 9, 1];
+        bubble_sort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+}
+
